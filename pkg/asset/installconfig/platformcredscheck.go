7d3b1ec4ef15a348a57d265a7cfdd276f520940d
Veneanar <veneanar@sylvanasgarde.com>
2019-07-08 11:36:00 +0200

    Update platformcredscheck.go

--- pkg/asset/installconfig/platformcredscheck.go
+++ pkg/asset/installconfig/platformcredscheck.go
@@ -5,9 +5,7 @@ import (
 
 	"github.com/gophercloud/utils/openstack/clientconfig"
 	"github.com/openshift/installer/pkg/asset"
-	awsconfig "github.com/openshift/installer/pkg/asset/installconfig/aws"
 	azureconfig "github.com/openshift/installer/pkg/asset/installconfig/azure"
-	"github.com/openshift/installer/pkg/types/aws"
 	"github.com/openshift/installer/pkg/types/azure"
 	"github.com/openshift/installer/pkg/types/libvirt"
 	"github.com/openshift/installer/pkg/types/none"
@@ -38,15 +36,6 @@ func (a *PlatformCredsCheck) Generate(dependencies asset.Parents) error {
 	var err error
 	platform := ic.Config.Platform.Name()
 	switch platform {
-	case aws.Name:
-		ssn, err := awsconfig.GetSession()
-		if err != nil {
-			return errors.Wrap(err, "creating AWS session")
-		}
-		err = awsconfig.ValidateCreds(ssn)
-		if err != nil {
-			return errors.Wrap(err, "validate AWS credentials")
-		}
 	case openstack.Name:
 		opts := new(clientconfig.ClientOpts)
 		opts.Cloud = ic.Config.Platform.OpenStack.Cloud
